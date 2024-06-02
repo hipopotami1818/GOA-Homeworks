2fe49d488f962b7bcee9d98546263736e8893577
hipopotami1818 <nikoloz.kakashvili@gmail.com>
2024-06-02 15:19:19 +0400

    commit

---
+++ level 3/home vory/homevory.py
@@ -0,0 +1,34 @@
+neim = "nikoloz"
+sorneim = "kakashvili"
+age = 11
+hobby = "football"
+country = "georgia"
+
+print(neim + " " + sorneim + ". i am " + str(age) + " years old." + " my hobby is " + hobby + ". i am from " + country  + ".")
+
+
+
+
+
+
+
+print(100 * 200)
+print(200 + 34 )
+print(1000001 // 4)
+print(100 / 4)
+print(23 - 20)
+print(500 / 5)
+print(5.5 - 1.5)
+print(100 / 4)
+print(23.67 * 290.00)
+print(9.8 / 5.3)
+
+
+
+
+
+
+
+
+
+
